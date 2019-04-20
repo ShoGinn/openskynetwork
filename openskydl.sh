54bc572df4051d114740399b9db5d5c1e871b1eb
ShoGinn <scottginn@Mac-mini.home>
2019-04-19 18:01:08 -0700

    Update openskydl.sh

--- openskydl.sh
+++ openskydl.sh
@@ -3,15 +3,15 @@ set -e
 
 if [ "$ARCH" = "arm" ] ; then
 	echo "Download armhf version"
-	curl --output opensky-feeder.deb "https://opensky-network.org/files/firmware/opensky-feeder_latest_armhf.deb"
+	curl --output /tmp/opensky-feeder.deb "https://opensky-network.org/files/firmware/opensky-feeder_latest_armhf.deb"
 fi
 if [ "$ARCH" = "arm64" ] ; then
     echo "Download arm64 version"
-    curl --output opensky-feeder.deb "https://opensky-network.org/files/firmware/opensky-feeder_latest_arm64.deb"
+    curl --output /tmp/opensky-feeder.deb "https://opensky-network.org/files/firmware/opensky-feeder_latest_arm64.deb"
 fi
 if [ "$ARCH" = "amd64" ] ; then
 	echo "Download AMD64 version"
-	curl --output opensky-feeder.deb "https://opensky-network.org/files/firmware/opensky-feeder_latest_amd64.deb"
+	curl --output /tmp/opensky-feeder.deb "https://opensky-network.org/files/firmware/opensky-feeder_latest_amd64.deb"
 fi
 
 echo 'opensky-feeder openskyd/latitude string 2' >> /tmp/preseed.txt; \

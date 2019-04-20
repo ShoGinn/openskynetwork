1fb6f12142f482fd6cfc70c7ad42b7f3285909e1
ShoGinn <scottginn@Mac-mini.home>
2019-04-19 21:36:23 -0700

    Update openskydl.sh

--- openskydl.sh
+++ openskydl.sh
@@ -5,7 +5,7 @@ if [ "$ARCH" = "arm" ] ; then
 	echo "Download armhf version"
 	curl --output /tmp/opensky-feeder.deb "https://opensky-network.org/files/firmware/opensky-feeder_latest_armhf.deb"
 fi
-if [ "$ARCH" = "arm64" ] ; then
+if [ "$ARCH" = "aarch64" ] ; then
     echo "Download arm64 version"
     curl --output /tmp/opensky-feeder.deb "https://opensky-network.org/files/firmware/opensky-feeder_latest_arm64.deb"
 fi
